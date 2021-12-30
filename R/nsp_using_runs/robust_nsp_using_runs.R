859d4178a2df7676d8c4064769a94458f42f2a06
Shakeel95 <41747714+Shakeel95@users.noreply.github.com>
2021-12-30 10:44:56 +0000

    declare changepoint if not monotonicity-correct fit is possible

--- R/nsp_using_runs/robust_nsp_using_runs.R
+++ R/nsp_using_runs/robust_nsp_using_runs.R
@@ -163,10 +163,15 @@ runs.test <- function(yy, thresh, deg)
   
   if (max.runs >= length(yy)) return(0)
   
+  
+  no.fits <- TRUE
+  
   test.non.increasing <- get.bounds.non.increasing(yy, max.runs)
   
   if (test.non.increasing$shape.check) 
   {
+    
+    no.fits <- FALSE
     
     if (deg == 0)
     {
@@ -192,6 +197,9 @@ runs.test <- function(yy, thresh, deg)
   
   if (test.non.decreasing$shape.check)
   {
+    
+    no.fits <- FALSE
+    
     if (deg == 0)
     {
       
@@ -210,9 +218,13 @@ runs.test <- function(yy, thresh, deg)
     }
   }
   
+  
+  if (no.fits) return(1)
+  
   return(0)
   
 }
+
 
 runs.test.experimental <- function(yy, thresh) 
 {
